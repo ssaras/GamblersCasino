3ff9b8feaa6aab5f6b350efb7e0452feb8263ecc
ssaras <shawnsaras@gmail.com>
2019-10-26 14:12:49 -0400

    documented methods

--- GamblersCasino/Program.cs
+++ GamblersCasino/Program.cs
@@ -20,16 +20,10 @@ namespace GamblersCasino
             Console.WriteLine("Enter hands");
 
             for (int i = 0; i < playerCount; i++)
-            {                
-                string input = Console.ReadLine();
-
-                List<int> cards = ConvertToCard(input);
-                List<int> suits = ConvertToSuit(input);
-
-                CardResult playerHand = FindHand(cards, suits);
+            {
+                CardResult playerHand = FindHand(Console.ReadLine());
                 playerHand.PlayerId = i;
                 playerHands.Add(playerHand);
-
             }
 
             List<CardResult> winner = FindBestHand(playerHands);
@@ -39,8 +33,6 @@ namespace GamblersCasino
             foreach (CardResult card in winner)
             {
                 output += card.PlayerId + " ";
-
-
             }
 
             Console.WriteLine("");
@@ -48,6 +40,15 @@ namespace GamblersCasino
             Console.ReadLine();
         }
 
+        /// <summary>
+        ///     Finds the best hand out of all the players hands
+        /// </summary>
+        /// <param name="cards">
+        ///     The players' hands
+        /// </param> 
+        /// <returns>
+        ///     A list with the highest hand. If there are more than one in the list, there is a tie
+        /// </returns>
         static List<CardResult> FindBestHand(List<CardResult> playerHands)
         {
             playerHands.Sort((x, y) => x.HandType.CompareTo(y.HandType));
@@ -155,12 +156,16 @@ namespace GamblersCasino
         }
 
         /// <summary>
-        /// Recursive function that finds the hand with the highest card in a set of hands.
+        ///     Recursive function that finds the hand with the highest card in a set of hands.
         /// </summary>
-        /// <param name="cards"></param> The hands.
-        /// <param name="highCardIndex"></param> The index of the highest card to check against
+        /// <param name="cards">
+        ///     The hands
+        /// </param> 
+        /// <param name="highCardIndex">
+        ///     The index of the highest card to check against
+        /// </param>
         /// <returns>
-        /// A list with the highest hand. If there are more than one in the list, there is a tie
+        ///     A list with the highest hand. If there are more than one in the list, there is a tie
         /// </returns>
         static List<CardResult> GetHighCard(List<CardResult> cards, int highCardIndex = 2)
         {
@@ -201,8 +206,17 @@ namespace GamblersCasino
 
         }
 
-        static CardResult FindHand(List<int> cards, List<int> suits)
+        /// <summary>
+        /// Finds the best hand from stdin
+        /// </summary>
+        /// <param name="input">stdin</param>
+        /// <returns>CardResult</returns>
+        static CardResult FindHand(string input)
         {
+
+            List<int> cards = ConvertToCard(input);
+            List<int> suits = ConvertToSuit(input);
+
             CardResult playerHand = IsStrightFlush(cards, suits);
             if (playerHand.HasHand)
             {
@@ -250,6 +264,12 @@ namespace GamblersCasino
             return playerHand;
         }
 
+
+        /// <summary>
+        /// Finds a stright in a hand. Also used to find straight flush.
+        /// </summary>
+        /// <param name="cards">the hand</param>
+        /// <returns>CardResult</returns>
         static CardResult IsStraight(List<int> cards)
         {
             int[] cardsSorted = SortHand(cards);
@@ -298,6 +318,11 @@ namespace GamblersCasino
             return cardResult;
         }
 
+        /// <summary>
+        /// Finds a stright flush in a hand.
+        /// </summary>
+        /// <param name="cards">the hand</param>
+        /// <returns>CardResult</returns>
         static CardResult IsStrightFlush(List<int> cards, List<int> suits)
         {
             // Check for Straight
@@ -325,6 +350,11 @@ namespace GamblersCasino
             return cardResult;
         }
 
+        /// <summary>
+        /// Find a matching set of 3 values in a hand. Used for finding flushes and three of a kinds
+        /// </summary>
+        /// <param name="cards">the hand</param>
+        /// <returns>CardResult</returns>
         static CardResult HasThreeMatchingCards(List<int> cards)
         {
             CardResult cardResult = new CardResult();
@@ -339,6 +369,11 @@ namespace GamblersCasino
             return cardResult;
         }
 
+        /// <summary>
+        /// Find a flush in a hand
+        /// </summary>
+        /// <param name="cards">the hand</param>
+        /// <returns>CardResult</returns>
         static CardResult IsFlush(List<int> cards)
         {
             CardResult cardResult = HasThreeMatchingCards(cards);
@@ -352,6 +387,11 @@ namespace GamblersCasino
             return cardResult;
         }
 
+        /// <summary>
+        /// Find a three of a kind in a hand
+        /// </summary>
+        /// <param name="cards">the hand</param>
+        /// <returns>CardResult</returns>
         static CardResult IsThreeKind(List<int> cards)
         {
             CardResult cardResult = HasThreeMatchingCards(cards);
@@ -365,6 +405,11 @@ namespace GamblersCasino
             return cardResult;
         }
 
+        /// <summary>
+        /// Finds a pair in a hands
+        /// </summary>
+        /// <param name="cards">the set of hands</param>
+        /// <returns>CardResult</returns>
         static CardResult IsPair(List<int> cards)
         {
             CardResult cardResult = new CardResult();
@@ -381,11 +426,25 @@ namespace GamblersCasino
             return cardResult;
         }
 
+        /// <summary>
+        /// Sorts a hand from low to high
+        /// </summary>
+        /// <param name="cards">the hand to be sorted</param>
+        /// <returns>
+        /// a sorted array
+        /// </returns>
         static int[] SortHand(List<int> cards)
         {
             return cards.OrderBy(i => i).ToArray();
         }
 
+        /// <summary>
+        /// Reurns the highest card in a hand
+        /// </summary>
+        /// <param name="cards">the set of hands</param>
+        /// <returns>
+        /// the highest hand
+        /// </returns>
         static CardResult GetHighCard(List<int> cards)
         {
             int[] cardsSorted = SortHand(cards);
@@ -401,12 +460,27 @@ namespace GamblersCasino
             return cardResult;
         }
         
+        /// <summary>
+        /// Generates a random number between specified values
+        /// </summary>
+        /// <param name="low">the lowest number possible</param>
+        /// <param name="high">The highest number possible</param>
+        /// <returns>
+        /// An int
+        /// </returns>
         static int GetRandomNumber(int low, int high)
         {
             Random random = new Random();
             return random.Next(low, high);
         }
 
+        /// <summary>
+        /// Converts suits to ints
+        /// </summary>
+        /// <param name="input">stdin</param>
+        /// <returns>
+        /// A list of ints.
+        /// </returns>
         static List<int> ConvertToSuit(string input)
         {
             List<int> cards = new List<int>();
@@ -449,6 +523,13 @@ namespace GamblersCasino
             return cards;
         }
 
+        /// <summary>
+        /// Convert face cards to ints.
+        /// </summary>
+        /// <param name="input">stdin</param>
+        /// <returns>
+        /// a list of ints
+        /// </returns>
         static List<int> ConvertToCard(string input)
         {
             List<int> cards = new List<int>();
